10fda762a354d6d803982630b4fb46f0b680905c
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2022-11-18 17:46:10 +0000

    editted some HTML elements and CSS. further developed Javascript also

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,81 @@ document.addEventListener("DOMContentLoaded", function() {
         if (this.getAttribute("data-type") === "replay") {
             replay();
         } else {
-            let userChoice = this.getAttribute("data-type");
+           userChoice = this.getAttribute("data-type");
             // console.log(userChoice); to check console is logging input// 
-            runGame()
-        } 
-        let playerScore = 0;
-        let compScore = 0;  
+            generatedAnswer ();
+        }   
         })
     }
 })
 
 function generatedAnswer() {
-    let choice = ["rock","paper","scissors","lizard","spock", "replay"];
-    let compChoice = ["rock","paper","scissors","lizard","spock"];
+    let choice = ["rock ","paper ","scissors ","lizard ","spock ", "replay "];
+    let compChoice = ["rock ","paper ","scissors ","lizard ","spock"];
     // make sure replay is index 5 so it isnt selected as a response to the game //
     let generatedAnswer = Math.floor(Math.random() * 5);
     
-    compChoice = (choice[generatedAnswer]);
+    compGen = (choice[generatedAnswer]);
+    compSelected = compGen;
+    userSelected = userChoice;
+    console.log(compGen);
+    console.log(userSelected);
+    compareAnswer ();
+
+
 }
+
+let playerScore = 0;
+let compScore = 0;
 
 function compareAnswer() {
 
+
+    //document.getElementById("operand1").innerHTML = 'Player'; move to 
+    // document.getElementById("operand2").innerHTML = 'Computer'; move to 
+
+if (userSelected === compSelected){
+        operand1.textContent = userSelected;
+        operand2.textContent = compSelected;
+        result.textContent = 'TIE';
+    }
+    else if(userSelected == 'rock'){
+        if(compGen == 'paper'){
+            document.getElementById("operand1").innerHTML = userSelected;
+            document.getElementById("operand2").innerHTML = compSelected;
+            result.textContent = 'Computer Wins!';
+            ++compScore;
+            console.log(compScore)
+            document.getElementById("cScore").innerHTML = compScore;
+            
+
+        }else{
+           document.getElementById("operand1").innerHTML = userSelected;
+           document.getElementById("operand2").innerHTML = compSelected;
+           result.textContent = 'Player Wins!';
+           ++playerScore;
+           console.log(playerScore)
+           document.getElementById("pScore").innerHTML = playerScore;
+        }
+    }
+    else if(userSelected == 'scissors'){
+        if(compGen == 'rock'){
+            document.getElementById("operand1").innerHTML = userSelected;
+            document.getElementById("operand2").innerHTML = compSelected;
+            result.textContent = 'Computer Wins!';
+            ++compScore;
+            console.log(compScore)
+        }else{
+            document.getElementById("operand1").innerHTML = userSelected;
+           document.getElementById("operand2").innerHTML = compSelected;
+           result.textContent = 'Player Wins!';
+           ++playerScore;
+           console.log(playerScore)
+        } 
+
+        
 };
+}
 
 function incrementUserScore() {
 
