63818ace8bae6316e31cd88c64ddbb60290c0003
JoshMc-Code <josh.mccoo@hotmail.co.uk>
2022-11-19 20:58:00 +0000

    completed Javascript for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,25 +13,31 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
+let playerScore = 0;
+let compScore = 0;
+
 
 function generatedAnswer() {
-    let choice = ["rock ","paper ","scissors ","lizard ","spock ", "replay "];
-    let compChoice = ["rock ","paper ","scissors ","lizard ","spock"];
+    
+    let choice = ["rock","paper","scissors","lizard","spock", "replay"];
     // make sure replay is index 5 so it isnt selected as a response to the game //
     let generatedAnswer = Math.floor(Math.random() * 5);
-    
+
     compGen = (choice[generatedAnswer]);
     compSelected = compGen;
     userSelected = userChoice;
     console.log(compGen);
     console.log(userSelected);
     compareAnswer ();
+    document.getElementById("operand1").innerHTML = userSelected;
+    document.getElementById("operand2").innerHTML = compSelected;
+    document.getElementById("pScore").innerHTML = playerScore;
+    document.getElementById("cScore").innerHTML = compScore;
 
 
 }
 
-let playerScore = 0;
-let compScore = 0;
+
 
 function compareAnswer() {
 
@@ -45,18 +51,12 @@ if (userSelected === compSelected){
         result.textContent = 'TIE';
     }
     else if(userSelected == 'rock'){
-        if(compGen == 'paper'){
-            document.getElementById("operand1").innerHTML = userSelected;
-            document.getElementById("operand2").innerHTML = compSelected;
+        if(compSelected == 'paper' || compSelected =='spock'){
             result.textContent = 'Computer Wins!';
             ++compScore;
             console.log(compScore)
-            document.getElementById("cScore").innerHTML = compScore;
             
-
         }else{
-           document.getElementById("operand1").innerHTML = userSelected;
-           document.getElementById("operand2").innerHTML = compSelected;
            result.textContent = 'Player Wins!';
            ++playerScore;
            console.log(playerScore)
@@ -64,36 +64,88 @@ if (userSelected === compSelected){
         }
     }
     else if(userSelected == 'scissors'){
-        if(compGen == 'rock'){
-            document.getElementById("operand1").innerHTML = userSelected;
-            document.getElementById("operand2").innerHTML = compSelected;
+        if(compSelected == 'rock' || compSelected =='spock'){
             result.textContent = 'Computer Wins!';
             ++compScore;
             console.log(compScore)
         }else{
-            document.getElementById("operand1").innerHTML = userSelected;
-           document.getElementById("operand2").innerHTML = compSelected;
            result.textContent = 'Player Wins!';
            ++playerScore;
            console.log(playerScore)
         } 
+    }
+    else if(userSelected == 'paper'){
+        if(compSelected == 'scissors' || compSelected =='lizard'){
+            result.textContent = 'Computer Wins!';
+            ++compScore;
+            console.log(compScore)
+        }else{
+           result.textContent = 'Player Wins!';
+           ++playerScore;
+           console.log(playerScore)
+        } 
+    }
+    else if(userSelected == 'lizard'){
+        if(compSelected == 'scissors' || compSelected =='rock'){
+            result.textContent = 'Computer Wins!';
+            ++compScore;
+            console.log(compScore)
+        }else{
+           result.textContent = 'Player Wins!';
+           ++playerScore;
+           console.log(playerScore)
+        } 
+    }
+    else if(userSelected == 'spock'){
+        if(compSelected == 'lizard' || compSelected =='paper'){
+            result.textContent = 'Computer Wins!';
+            ++compScore;
+            console.log(compScore)
+        }else{
+           result.textContent = 'Player Wins!';
+           ++playerScore;
+           console.log(playerScore)
+        } 
+    }
 
-        
-};
+    finalPScore = playerScore;
+    finalCompScore = compScore;
+    console.log(finalPScore);
+
+    if(finalPScore == 5 || finalCompScore == 5){
+        gameOver();
+    }
 }
 
-function incrementUserScore() {
 
-};
 
-function incrementCompScore() {
+function gameOver() {
+    finalPScore = playerScore;
+    finalCompScore = compScore;
+    console.log(finalPScore);
+    console.log(finalCompScore);
+    let chooseMove = document.querySelector('.controls-area');
+    let result = document.querySelector('.results-area');
+    let replayBtn = document.querySelector('.replay');
 
-};
+        chooseMove.innerText = 'Game Over!!'
 
-function checkScores() {
+            if(finalPScore > finalCompScore){
+                result.innerText = 'You Won The Game'
+                result.style.color = '#308D46';
+                replayGame();
+            }
+            else if(finalPScore < finalCompScore){
+                result.innerText = 'You Lost The Game';
+                result.style.color = 'red';
+                replayGame();
+            };
+}
 
-};
+function replayGame() {
+    let replayBtn = document.querySelector('.replay');
+    replayBtn.addEventListener('click',() => {
+        window.location.reload();
+    });
 
-function replay() {
-console.log('replay')
-};
+}
